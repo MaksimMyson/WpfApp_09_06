1f83669b63ae1fefdc38033a4913dbe46650bfc3
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-09 13:04:48 +0300

    was done ex7

--- WpfApp_09_06/MainWindow.xaml.cs
+++ WpfApp_09_06/MainWindow.xaml.cs
@@ -1,24 +1,66 @@
-﻿using System.Text;
+﻿using System;
+using System.Linq;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp_09_06
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Mutex mutex = new Mutex();
+        private  int[] array = new int[10];
+        private Random random = new Random();
+        private int maxValue;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            OutputTextBox.Clear();
+            InitializeArray();
+            Task.Run(() => FirstThread());
+        }
+
+        private void InitializeArray()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+
+            Dispatcher.Invoke(() =>
+            {
+                OutputTextBox.AppendText("Initial array: " + string.Join(", ", array) + "\n");
+            });
+        }
+
+        private void FirstThread()
+        {
+            mutex.WaitOne();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += random.Next(1, 10);
+                Thread.Sleep(100); // Затримка для демонстрації
+            }
+            mutex.ReleaseMutex();
+            Task.Run(() => SecondThread());
+        }
+
+        private void SecondThread()
+        {
+            mutex.WaitOne();
+            maxValue = array.Max();
+            mutex.ReleaseMutex();
+            Dispatcher.Invoke(DisplayResults);
+        }
+
+        private void DisplayResults()
+        {
+            OutputTextBox.AppendText("Modified array: " + string.Join(", ", array) + "\n");
+            OutputTextBox.AppendText("Max value: " + maxValue + "\n");
         }
     }
 }
